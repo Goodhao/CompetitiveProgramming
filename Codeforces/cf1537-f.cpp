a77762fa4b93e7197f9ed05ce62bfa4556c473f0
Goodhao <goodhao2012@gmail.com>
2021-12-15 08:46:58 +0800

    solve F

--- Codeforces/cf1537-f.cpp
+++ Codeforces/cf1537-f.cpp
@@ -7,14 +7,16 @@ const int N=5*1e5+10;
 int T;
 int n,m;
 ll v[N],t[N];
-int fa[N],d[N];
+bool vis[N];
 vector<int> g[N];
-void dfs(int x,int f) {
-	fa[x]=f;
+vector<pair<int,int>> e;
+int color[N];
+void dfs(int x,int c) {
+	vis[x]=1;
+	color[x]=c;
 	for (auto y : g[x]) {
-		if (y!=f) {
-			d[x]++;
-			dfs(y,x);
+		if (!vis[y]) {
+			dfs(y,~c);
 		}
 	}
 }
@@ -22,43 +24,48 @@ int main() {
 	cin>>T;
 	while (T--) {
 		cin>>n>>m;
-		for (int i=1;i<=n;i++) d[i]=0;
 		for (int i=1;i<=n;i++) cin>>v[i];
 		for (int i=1;i<=n;i++) cin>>t[i];
+		for (int i=1;i<=n;i++) g[i].clear();
+		for (int i=1;i<=n;i++) color[i]=0,vis[i]=0;
+		e.clear();
 		for (int i=1;i<=m;i++) {
 			int x,y;
 			cin>>x>>y;
+			e.push_back(make_pair(x,y));
 			g[x].push_back(y);
 			g[y].push_back(x);
 		}
-		if (m>=n) {
-			if (n==2) {
-				if (t[1]-t[2]==v[1]-v[2]) cout<<"YES"<<endl;
-				else cout<<"NO"<<endl;
-			} else cout<<"YES"<<endl;
-		} else {
-			// m=n-1
-			bool ok=0;
-			dfs(1,0);
-			queue<int> q;
-			for (int i=1;i<=n;i++) if (d[i]==0) q.push(i);
-			while (!q.empty()) {
-				int u=q.front();
-				q.pop();
-				ll dd=t[u]-v[u];
-				if (u==1) {
-					// root
-					if (dd==0) {
-						ok=1;
-					}
-					break;
-				}
-				v[u]+=dd;
-				v[fa[u]]+=dd;
-				if (--d[fa[u]]==0) q.push(fa[u]);
+		dfs(1,0);
+		bool check=0;
+		ll ts=0,vs=0;
+		for (int i=1;i<=n;i++) ts+=t[i],vs+=v[i];
+		if ((ts-vs)%2) {
+			cout<<"NO"<<endl;
+			check=1;
+		}
+		if (!check) for (auto ee : e) {
+			int x = ee.first, y = ee.second;
+			if (color[x]==color[y]) {
+				// non-bipartite
+				cout<<"YES"<<endl;
+				check=1;
+				break;
 			}
-			if (ok) cout<<"YES"<<endl;
-			else cout<<"NO"<<endl;
+		}
+		if (!check) {
+			ll s1=0,s2=0;
+			for (int i=1;i<=n;i++) {
+				if (color[i]==0) s1+=t[i]-v[i];
+				else s2+=t[i]-v[i];
+			}
+			if (s1==s2) {
+				cout<<"YES"<<endl;
+				check=1;
+			} else {
+				cout<<"NO"<<endl;
+				check=1;
+			}
 		}
 	}
 	return 0;
